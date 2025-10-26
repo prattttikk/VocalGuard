05cef8c220aa60e2ba442d6b6f433fd31dcb81c7
prattttikk <bpratik3010@gmail.com>
2025-10-26 18:03:44 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View style={styles.container}>
+      {/* Render the new Login screen for preview. Replace with your navigation stack when ready. */}
+      <LoginScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ declarations.d.ts
@@ -0,0 +1,3 @@
+declare module '@react-native-picker/picker';
+declare module 'react-native-vector-icons/Feather';
+declare module 'react-native-vector-icons/Ionicons';
